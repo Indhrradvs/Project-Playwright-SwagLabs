f3c63d49784d3dd84bf6d5520e11e0ba99c68072
Venkat <dvsphaneendra@gmail.com>
2026-09-03 21:46:59 -0400

    feat: complete CheckoutStepTwoPage and CheckoutCompletePage with full test coverage

--- pages/checkout_step_two_page.py
+++ pages/checkout_step_two_page.py
@@ -7,3 +7,15 @@ class CheckoutStepTwoPage(BasePage):
         super().__init__(my_page)
 
         self.checkout_label = my_page.locator("[data-test='title']")
+        self.payment_info = my_page.locator('[data-test="payment-info-label"]')
+        self.payment_card_info = my_page.locator('[data-test="payment-info-value"]')
+        self.shipping_info = my_page.locator('[data-test="shipping-info-label"]')
+        self.shipping_delivery_info = my_page.locator(
+            '[data-test="shipping-info-value"]'
+        )
+        self.price_total = my_page.locator('[data-test="total-info-label"]')
+        self.item_total = my_page.locator('[data-test="subtotal-label"]')
+        self.tax_label = my_page.locator('[data-test="tax-label"]')
+        self.total_amount = my_page.locator('[data-test="total-label"]')
+        # button
+        self.finish_btn = my_page.locator('[data-test="finish"]')
